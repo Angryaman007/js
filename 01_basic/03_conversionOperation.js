3c6cd4aa5457295d6de1d5406695fddbbb52dae2
Amandeep Singhe <pks95990@gmail.com>
2024-05-05 00:06:38 +0530

    lol

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,7 +1,29 @@
-let score="33abc"
-console.log(typeof score);
-console.log(typeof(score));
+let score=null
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" =>Nan
+// true=> 1; false => 0
+
+//let isLoggedIn = "aman"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0=> false
+//"" => false
+//"aman" => true 
+let someNumber =34
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
