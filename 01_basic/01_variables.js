f35e4180218e47c6747f14af42094bd2eb09da9f
Amandeep Singhe <pks95990@gmail.com>
2024-05-04 20:45:41 +0530

    changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail ="angryaman07.com"
+var accountPassword ="1122"
+accountCity ="delhi"
+let accountState
+
+
+// accountId = 2 // not allowed//
+
+
+accountEmail="aman01.com"
+accountPassword="1234"
+accountCity="up"
+
+/* prefer not to use var because of issue in block scope and fuctional scope */
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
+
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version 
+ 
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+6);
+
+console.log("Aman")
+
+let name="nani"
+let age =22
+let isLoggedIN=false
+let state =null
+
+
+/* 
+
+number=>2 to power 53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined=>""
+symbol=>unique
+object
+
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
