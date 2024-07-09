de545cb1c4bbb31c3964156b8d42e70a39c091c8
Amandeep Singhe <pks95990@gmail.com>
2024-07-09 18:55:37 +0530

    commit1

---
+++ 04_advance(control flow)/one.js
@@ -0,0 +1,73 @@
+// if
+
+// <,  >, <=, >=, ==, !=, ===, !==
+
+
+// const isUserLoggedin=true
+// const temperature=51
+
+/*
+if(temperature<50){
+console.log("lesss than 50");
+}
+else{
+console.log("temperature is greater then 50");
+}
+
+console.log("excute")
+*/
+
+/*
+const score=200
+
+if(score>100)
+{
+    const power="fly"
+    console.log(`user power: ${power}`)
+
+}
+
+console.log(`user power: ${power}`)
+
+*/
+
+// const balance=1000
+/*
+if(balance>500)
+{
+    console.log("test")
+}
+*/
+
+/*
+if(balance<500)
+{
+    console.log("less than")
+}
+else if( balance<700)
+{
+    console.log("less than 750")
+}
+else if(balance<900)
+{
+    consol.log("less than 800")
+}
+else 
+{
+    console.log("less than 1200")
+}
+    */
+
+const isUserLoggedin=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(isUserLoggedin && debitCard && 2==3)
+{
+    console.log("allow to buy course")
+
+}
+if(loggedInFromEmail || loggedInFromEmail )
+{
+    console.log("user logged In")
+}  
