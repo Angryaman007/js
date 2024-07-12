4be7d6a17cd51b2bddc153f669136c5728db8b57
Amandeep Singhe <pks95990@gmail.com>
2024-07-12 20:39:15 +0530

    commit1

---
+++ 04_advance(control flow)/truthy_or_falsy.js
@@ -0,0 +1,41 @@
+
+const userEmail="amansingh007@gmail.com"
+// const userEmail="amansingh007@"
+// const userEmail=""
+// const userEmail=[]
+// const userEmail={}
+
+if(userEmail)
+{
+    console.log("got user email")
+
+}
+else
+{
+console.log("don't have user email")
+}
+
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//tuthy values
+// "0", 'false', " ", [], {}, fuction(){}, 
+
+
+// how to find out if it is arry or not 
+
+/*
+if(userEmail.length===0)
+{
+    console.log("array is empty")
+}
+*/
+
+const emptyObj={}
+    if(object.keys(emptyObj).length===0)
+    {
+        console.log("object is empty")
+
+    } 
