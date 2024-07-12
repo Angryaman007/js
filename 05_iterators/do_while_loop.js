57a658bd9bf0cb3bbebe9d5fb9efe8e370b3fd7c
Amandeep Singhe <pks95990@gmail.com>
2024-07-12 20:57:08 +0530

    commit1

---
+++ 05_iterators/do_while_loop.js
@@ -0,0 +1,30 @@
+// while loop
+
+let index=0
+while (index<=30) {
+    console.log(`value of index is ${index}`)
+    index=index +3
+}
+
+console.log(" ")
+
+
+let myArray=["flash", "superman", "wonderwomen", "ben10"]
+let arr=0
+while(arr< myArray.length)
+{
+    console.log(`value is ${myArray[arr]}`)
+    arr=arr+1
+}
+
+console.log(" ")
+
+// dowhile loop ( do "kaam pahele hota hai " while "conditon baad mein checkhoti hai ")
+
+
+// let score=1
+let score=11
+do {
+    console.log(`scorer is ${score}`)
+score++
+} while (score<=10);
